6e7cf650c14aba1c294df8ffa0891b55fcf3deed
yangyang <youngqiankun@163.com>
2017-09-04 13:19:54 +0800

    duapte

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ rm /home/dogmell/jetty/webapps/root.war
 echo '******rm root.war end******'
 
 echo '******cp root.war start******'
-cp target/root.war /home/dogmell/jetty/webapps
+cp target/root.war /home/yangzhengren/jetty/webapps
 echo '******cp root.war end******'
 
 echo '******jetty restart******'
