bd75ecfa972b4ba8b0cde6db4814e8bb09d21d1e
yangyang <youngqiankun@163.com>
2017-09-04 13:09:39 +0800

    udpate

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
-
+echo '******package start******'
 mvn clean package -Dmaven.test.skip=true
+echo '******package end******'
 
+echo '******rm root.war start******'
 rm /home/dogmell/jetty/webapps/root.war
+echo '******rm root.war end******'
 
+echo '******cp root.war start******'
 cp target/root.war /home/dogmell/jetty/webapps
+echo '******cp root.war end******'
+
+echo '******jetty restart******'
+sh /home/yangzhengren/jetty/bin/jetty.sh restart
